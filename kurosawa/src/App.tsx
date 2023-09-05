859f7f33540eeb232c5d9f4b1514890e1ab08185
Brian David <bdavid1530@gmail.com>
2023-09-05 14:20:41 +0100

    set up url paths + home page

--- kurosawa/src/App.tsx
+++ kurosawa/src/App.tsx
@@ -1,10 +1,14 @@
+import {Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  
+  const location = useLocation();
 
   return (
     <>
-      
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<Home />}/>
+        </Routes>
     </>
   )
 }
---
+++ kurosawa/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.pageBg}>
+        
+    </div>
+  )
+}
+
+export default Home;
