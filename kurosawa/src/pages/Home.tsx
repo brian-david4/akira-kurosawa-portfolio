644b13aac7b49f7d5079ccd843d3e4d175c3aada
Brian David <bdavid1530@gmail.com>
2023-09-05 15:34:31 +0100

    Set Up conditional rendering on mouse enter + leave

--- kurosawa/src/pages/Home.tsx
+++ kurosawa/src/pages/Home.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import styles from "./Home.module.css";
 
 const Home = () => {
+    const [isFortiesActive, setIsFortiesActive] = useState(false);
+    const [isFiftiesActive, setIsFiftiesActive] = useState(false);
+    const [isSixtiesActive, setIsSixtiesActive] = useState(false);
+    const [isRestActive, setIsRestActive] = useState(false);
+
   return (
     <div className={styles.pageBg}>
-        <div className={styles.fortiesSection}></div>
-        <div className={styles.fiftiesSection}></div>
-        <div className={styles.sixtiesSection}></div>
-        <div className={styles.restOfSection}></div>
+        <div onMouseEnter={() => setIsFortiesActive(true)} onMouseLeave={() => setIsFortiesActive(false)} className={styles.fortiesSection}></div>
+        <div onMouseEnter={() => setIsFiftiesActive(true)} onMouseLeave={() => setIsFiftiesActive(false)} className={styles.fiftiesSection}></div>
+        <div onMouseEnter={() => setIsSixtiesActive(true)} onMouseLeave={() => setIsSixtiesActive(false)} className={styles.sixtiesSection}></div>
+        <div onMouseEnter={() => setIsRestActive(true)} onMouseLeave={() => setIsRestActive(false)} className={styles.restOfSection}></div>
+        
+        {isFortiesActive && <h1>40 is active</h1>}
+        {isFiftiesActive && <h1>50 is active</h1>}
+        {isSixtiesActive && <h1>60 is active</h1>}
+        {isRestActive && <h1>Rest is active</h1>}
     </div>
+
   )
 }
 
