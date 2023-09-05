aa687fa80d65b477c44f8c052ef21407a181b2e2
Brian David <bdavid1530@gmail.com>
2023-09-05 15:12:04 +0100

    Create divs for decades

--- kurosawa/src/pages/Home.tsx
+++ kurosawa/src/pages/Home.tsx
@@ -3,7 +3,10 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <div className={styles.pageBg}>
-        
+        <div className={styles.fortiesSection}></div>
+        <div className={styles.fiftiesSection}></div>
+        <div className={styles.sixtiesSection}></div>
+        <div className={styles.restOfSection}></div>
     </div>
   )
 }
