9bc89453db56d2e69ed54ff9645e7ca147b7b014
Brian David <bdavid1530@gmail.com>
2023-09-05 18:21:32 +0100

    images appear on decade div hover

--- kurosawa/src/pages/Home.tsx
+++ kurosawa/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import styles from "./Home.module.css";
+import highlow from "../images/highlow.webp";
+import mada from "../images/madadayo.webp";
+import seven from "../images/seven.webp";
+import yojimbo from "../images/yojimbo.webp"
 
 const Home = () => {
     const [isFortiesActive, setIsFortiesActive] = useState(false);
@@ -14,10 +18,18 @@ const Home = () => {
         <div onMouseEnter={() => setIsSixtiesActive(true)} onMouseLeave={() => setIsSixtiesActive(false)} className={styles.sixtiesSection}></div>
         <div onMouseEnter={() => setIsRestActive(true)} onMouseLeave={() => setIsRestActive(false)} className={styles.restOfSection}></div>
         
-        {isFortiesActive && <h1>40 is active</h1>}
-        {isFiftiesActive && <h1>50 is active</h1>}
-        {isSixtiesActive && <h1>60 is active</h1>}
-        {isRestActive && <h1>Rest is active</h1>}
+        {isFortiesActive && 
+        <div className={`${styles.forties} ${styles.backgroundImage}`}><img src={seven}/></div> }
+        
+        {isFiftiesActive && 
+        <div className={`${styles.fifties} ${styles.backgroundImage}`}><img src={yojimbo}/></div> }
+        
+        {isSixtiesActive && 
+        <div className={`${styles.sixties} ${styles.backgroundImage}`}><img src={highlow}/></div>}
+        
+        {isRestActive && 
+        <div className={`${styles.rest} ${styles.backgroundImage}`}><img src={mada}/></div>}
+
     </div>
 
   )
