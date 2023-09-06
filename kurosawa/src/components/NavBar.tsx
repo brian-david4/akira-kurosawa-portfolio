6f8e2afbe25807d088f52c3f3e202f46bed7e82e
Brian David <bdavid1530@gmail.com>
2023-09-06 13:02:20 +0100

    Create NavBar links

--- kurosawa/src/components/NavBar.tsx
+++ kurosawa/src/components/NavBar.tsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom"
 import styles from "./NavBar.module.css"
 
 const NavBar = () => {
   return (
     <>
     <div className={styles.navbarWrapper}>
-        <div className={styles.navbar}></div>
+        <div className={styles.navbar}>
+            <Link className={styles.navbarLink} to="/">Early work's</Link>
+            <Link className={styles.navbarLink} to="/">International Success</Link>
+            <Link className={styles.navbarLink} to="/">In his Prime</Link>
+            <Link className={styles.navbarLink} to="/">Later years</Link>
+        </div>
     </div>
     </>
   )
