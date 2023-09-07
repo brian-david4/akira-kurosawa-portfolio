aaf5eb958c45774a5b98b15602e891423a660a92
Brian David <bdavid1530@gmail.com>
2023-09-07 23:37:04 +0100

    Video play on decade hover

--- kurosawa/src/pages/Home.tsx
+++ kurosawa/src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import styles from "./Home.module.css";
 import { AnimatePresence, motion } from "framer-motion";
-import highlow from "../images/highlow.webp";
-import mada from "../images/madadayo.webp";
-import seven from "../images/seven.webp";
-import yojimbo from "../images/yojimbo.webp";
-import ran from "../images/ran1.webp";
 import { decadeImageFade } from "./HomeAnim";
 import Menu from "../components/Menu";
+// images
+import styles from "./Home.module.css";
+import forty from "../../public/1940.mp4";
+import fifty from "../../public/1950.mp4";
+import rest from "../../public/restFilm.mp4";
+import sixty from "../../public/1960.mp4"
+import ran from "../images/ran1.webp";
 
 const Home = () => {
     const [isFortiesActive, setIsFortiesActive] = useState(false);
@@ -29,46 +30,53 @@ const Home = () => {
         <div onMouseEnter={() => activeWrap(1)} className={styles.fiftiesSection}></div>
         <div onMouseEnter={() => activeWrap(2)} className={styles.sixtiesSection}></div>
         <div onMouseEnter={() => activeWrap(3)} className={styles.restOfSection}></div>
-        
+        {/* 1940's */}
         <AnimatePresence mode="wait">
             {isFortiesActive &&
             <>
-            <motion.div variants={decadeImageFade} initial="initial" animate="enter" exit="exit" className={styles.backgroundImage}><img src={seven}/></motion.div>
+            <motion.div variants={decadeImageFade} initial="initial" animate="enter" exit="exit" className={styles.backgroundImage}>
+                <video className={styles.fortyVideo} autoPlay playsInline muted loop draggable="false"><source src={forty}/></video>
+            </motion.div>
             <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit">40's</motion.h1>
             </>}
         </AnimatePresence>
-            
-            <AnimatePresence mode="wait">
-                {isFiftiesActive &&
-                <>
-                    <motion.div variants={decadeImageFade} initial="initial" animate="enter" exit="exit" className={styles.backgroundImage}><img src={yojimbo}/></motion.div>
-                    <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit">50's</motion.h1>
-                </>}
-            </AnimatePresence>
-            
-            <AnimatePresence mode="wait">
-                {isSixtiesActive &&
-                <>
-                    <motion.div variants={decadeImageFade} initial="initial" animate="enter" exit="exit" className={styles.backgroundImage}><img src={highlow}/></motion.div>
-                    <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit">60's</motion.h1>
-                </>}
-            </AnimatePresence>
-            
-            <AnimatePresence mode="wait">
-                {isRestActive &&
-                <>
-                    <motion.div variants={decadeImageFade} initial="initial" animate="enter" exit="exit" className={styles.backgroundImage}><img src={mada}/></motion.div>
-                    <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit">'70-98</motion.h1>
-                </>}
-            </AnimatePresence>
-
-            <AnimatePresence mode="wait">
-                {!isFortiesActive && !isFiftiesActive && !isSixtiesActive && !isRestActive && 
-                <>
-                    <motion.div variants={decadeImageFade} initial="initial" animate="enter" exit="exit" className={styles.backgroundImage}><img src={ran}/></motion.div>
-                    <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit"></motion.h1>
-                </>}
-            </AnimatePresence>
+        {/* 1950's */}
+        <AnimatePresence mode="wait">
+            {isFiftiesActive &&
+            <>
+                <div className={styles.backgroundImage}>
+                    <video className={styles.fiftyVideo} autoPlay playsInline muted loop draggable="false"><source src={fifty}/></video></div>
+                <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit">50's</motion.h1>
+            </>}
+        </AnimatePresence>
+        {/* 1960's */}
+        <AnimatePresence mode="wait">
+            {isSixtiesActive &&
+            <>
+                <div className={styles.backgroundImage}>
+                    <video className={styles.sixtyVideo} autoPlay playsInline muted loop draggable="false"><source src={sixty} /></video>
+                </div>
+                <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit">60's</motion.h1>
+            </>}
+        </AnimatePresence>
+        {/* rest */}
+        <AnimatePresence mode="wait">
+            {isRestActive &&
+            <>
+                <motion.div variants={decadeImageFade} initial="initial" animate="enter" exit="exit" className={styles.backgroundImage}>
+                    <video className={styles.restVideo} autoPlay playsInline muted loop draggable="false"><source src={rest}/></video>
+                </motion.div>
+                <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit">'70-98</motion.h1>
+            </>}
+        </AnimatePresence>
+        {/* default pic */}
+        <AnimatePresence mode="wait">
+            {!isFortiesActive && !isFiftiesActive && !isSixtiesActive && !isRestActive && 
+            <>
+                <motion.div variants={decadeImageFade} initial="initial" animate="enter" exit="exit" className={styles.backgroundImage}><img src={ran}/></motion.div>
+                <motion.h1 variants={decadeImageFade} initial="initial" animate="enter" exit="exit"></motion.h1>
+            </>}
+        </AnimatePresence>
 
     </div>
 
