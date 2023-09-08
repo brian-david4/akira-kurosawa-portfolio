fa5f5b1c779d69ce6263428af72dcb4a2284d56c
Brian David <bdavid1530@gmail.com>
2023-09-08 15:13:24 +0100

    NavLinkMenu appear on hover for "early works"

--- kurosawa/src/components/NavBar.tsx
+++ kurosawa/src/components/NavBar.tsx
@@ -1,17 +1,27 @@
-import { motion } from "framer-motion";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import styles from "./NavBar.module.css"
 import { linkFade, navbarScale } from "./NavBarAnim";
 
 const NavBar = () => {
+  const [earlyActive, setEarlyActive] = useState(false);
   return (
     <>
     <div className={styles.navbarWrapper}>
         <motion.div variants={navbarScale} initial="initial" animate="enter" exit="exit" className={styles.navbar}>
-            <motion.div variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>Early work's</motion.div>
-            <motion.div variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>International Success</motion.div>
+            <motion.div onMouseEnter={()=>setEarlyActive(true)} variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>Early work's</motion.div>
+            <motion.div variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>International Recognition</motion.div>
             <motion.div variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>In his Prime</motion.div>
             <motion.div variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>Later years</motion.div>
         </motion.div>
+        {/* early works */}
+        <AnimatePresence mode="wait">
+          {earlyActive && 
+          <motion.div
+            className={styles.early}
+            onMouseLeave={()=>setEarlyActive(false)}
+            ></motion.div>}
+        </AnimatePresence>
     </div>
     </>
   )
