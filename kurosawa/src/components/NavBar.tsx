08cbe7828763b169c41b41caa453262138f9fdf8
Brian David <bdavid1530@gmail.com>
2023-09-12 14:36:30 +0100

    image appear on navLinkHover (all)

--- kurosawa/src/components/NavBar.tsx
+++ kurosawa/src/components/NavBar.tsx
@@ -8,19 +8,25 @@ import great from "../images/highlow.webp";
 import later from "../images/madadayo.webp";
 
 const NavBar = () => {
-  const [earlyActive, setEarlyActive] = useState(false); 
+  const [earlyActive, setEarlyActive] = useState(false);
+  const [intlActive, setIntlActive] = useState(false);
+  const [greatActive, setGreatActive] = useState(false);
+  const [laterActive, setLaterActive] = useState(false); 
 
   return (
     <>
     <div className={styles.navbarWrapper}>
         <motion.div variants={navbarScale} initial="initial" animate="enter" exit="exit" className={styles.navbar}>
             <motion.div onMouseEnter={()=>setEarlyActive(true)} onMouseLeave={()=>setEarlyActive(false)} variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>Early work's</motion.div>
-            <motion.div variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>International Recognition</motion.div>
-            <motion.div variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>Greatness</motion.div>
-            <motion.div variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>Later years</motion.div>
+            <motion.div onMouseEnter={()=>setIntlActive(true)} onMouseLeave={()=>setIntlActive(false)} variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>International Recognition</motion.div>
+            <motion.div onMouseEnter={()=>setGreatActive(true)} onMouseLeave={()=>setGreatActive(false)} variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>Greatness</motion.div>
+            <motion.div onMouseEnter={()=>setLaterActive(true)} onMouseLeave={()=>setLaterActive(false)} variants={linkFade} initial="initial" animate="enter" exit="exit" className={styles.navbarLink}>Later years</motion.div>
         </motion.div>
 
-        {earlyActive && <img className={styles.navLinkImage} src={early} alt="Stray Dog"/>}
+        {earlyActive && <img id={styles.early} className={styles.navLinkImage} src={early} alt="Stray Dog"/>}
+        {intlActive && <img id={styles.intl} className={styles.navLinkImage} src={intl} alt="Yojimbo"/>}
+        {greatActive && <img id={styles.great} className={styles.navLinkImage} src={great} alt="High and Low"/>}
+        {laterActive && <img id={styles.later} className={styles.navLinkImage} src={later} alt="Madadayo"/>}
     </div>
     </>
   )
