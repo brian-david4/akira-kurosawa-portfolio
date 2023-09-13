8b5c8032b5e67f3fd2c99a103aed9bedf7e09cee
Brian David <bdavid1530@gmail.com>
2023-09-13 12:24:55 +0100

    Early works page title and signature

--- kurosawa/src/pages/EarlyWorks/EarlyWorks.tsx
+++ kurosawa/src/pages/EarlyWorks/EarlyWorks.tsx
@@ -1,7 +1,20 @@
+import signature from "../../images/signature.webp";
+import styles from "./Early.module.css";
+
 const EarlyWorks = () => {
   return (
     <>
-      <h1>Akira Kurosawa</h1>
+      <div className={styles.nameTitle}>
+        <h1>Early</h1>
+        <h1>Work's</h1>
+      </div>
+
+      <img
+        className={styles.signature}
+        src={signature}
+        alt="Akira Kurosawa's signature"
+        draggable="false"
+      />
     </>
   );
 };
