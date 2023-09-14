f51f4ff29a42f0ab95b7a3068d8530fc30e88c22
Brian David <bdavid1530@gmail.com>
2023-09-14 23:23:59 +0100

    Create image wide (suagato00)
    - change name hover translate to 50ms (from 100ms)
    - DecadeHeader width changed to max-content

--- kurosawa/src/pages/EarlyWorks/EarlyWorks.tsx
+++ kurosawa/src/pages/EarlyWorks/EarlyWorks.tsx
@@ -1,6 +1,7 @@
 import DecadePageHeader from "../../components/DecadePageHeader/DecadePageHeader";
 import KurosawaName from "../../components/KurosawaName/KurosawaName";
 import Signature from "../../components/Signature/Signature";
+import sugato00 from "../../images/Early-Works/sugato_00.webp";
 import styles from "./Early.module.css";
 
 const EarlyWorks = () => {
@@ -12,6 +13,12 @@ const EarlyWorks = () => {
         <h1>Early</h1>
         <h1>Work's</h1>
       </DecadePageHeader>
+
+      <section className={styles.introSection}>
+        <div className={styles.imgWide}>
+          <img src={sugato00} alt="Sanshiro Sugato" />
+        </div>
+      </section>
     </>
   );
 };
