09518d332e922c64154b1a31a4842d7bc30db658
Brian David <bdavid1530@gmail.com>
2023-09-18 22:15:55 +0100

    Nav menu renders on menuBtn click

--- kurosawa/src/components/DecadeNavBar/DecadeNavBar.tsx
+++ kurosawa/src/components/DecadeNavBar/DecadeNavBar.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import styles from "./DNavBar.module.css";
+import DecadeNavMenu from "./DecadeNavMenu";
 
 const DecadeNavBar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
   return (
-    <div className={styles.menuBtn} onClick={() => console.log("clicked")}>
-      <div className={styles.menuBtnInner}>
-        <span>m</span>
-        <span>e</span>
-        <span>n</span>
-        <span>u</span>
+    <>
+      <div
+        className={styles.menuBtn}
+        onClick={() => setMenuActive(!menuActive)}
+      >
+        <div className={styles.menuBtnInner}>
+          <span>m</span>
+          <span>e</span>
+          <span>n</span>
+          <span>u</span>
+        </div>
       </div>
-    </div>
+      {menuActive && <DecadeNavMenu />}
+    </>
   );
 };
 
---
+++ kurosawa/src/components/DecadeNavBar/DecadeNavMenu.tsx
@@ -0,0 +1,7 @@
+import styles from "./DNavBar.module.css";
+
+const DecadeNavMenu = () => {
+  return <div className={styles.navMenu}></div>;
+};
+
+export default DecadeNavMenu;
