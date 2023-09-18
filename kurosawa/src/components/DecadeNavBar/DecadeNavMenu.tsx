f7efd72e49ea632e5dc63e3141a2ed788305306d
Brian David <bdavid1530@gmail.com>
2023-09-18 23:39:40 +0100

    Create nav links for decade nav menu

--- kurosawa/src/components/DecadeNavBar/DecadeNavMenu.tsx
+++ kurosawa/src/components/DecadeNavBar/DecadeNavMenu.tsx
@@ -1,8 +1,17 @@
 import { motion } from "framer-motion";
 import styles from "./DNavBar.module.css";
 import { menuBtn } from "./Anims";
+import { Link } from "react-router-dom";
 
 const DecadeNavMenu = () => {
+  const links = [
+    "Home",
+    "Early Works",
+    "International Success",
+    "Greatness",
+    "Later Years",
+  ];
+
   return (
     <motion.div
       variants={menuBtn}
@@ -10,7 +19,15 @@ const DecadeNavMenu = () => {
       animate="enter"
       exit="exit"
       className={styles.navMenu}
-    ></motion.div>
+    >
+      <div className={styles.linksContainer}>
+        {links.map((link, index) => (
+          <div className={styles.link} key={index}>
+            <Link to="/">{link}</Link>
+          </div>
+        ))}
+      </div>
+    </motion.div>
   );
 };
 
