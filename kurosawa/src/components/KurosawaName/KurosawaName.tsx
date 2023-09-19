927579ecf8e467b282eee5bb28b5d0043ec07430
Brian David <bdavid1530@gmail.com>
2023-09-19 13:53:06 +0100

    Kurosawa name enter animation

--- kurosawa/src/components/KurosawaName/KurosawaName.tsx
+++ kurosawa/src/components/KurosawaName/KurosawaName.tsx
@@ -1,16 +1,31 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
 import styles from "./KName.module.css";
 
 const KurosawaName = () => {
+  useEffect(() => {
+    gsap.fromTo(
+      ".ltr",
+      { yPercent: -175 },
+      {
+        duration: 0.8,
+        yPercent: 0,
+        stagger: { amount: 0.6 },
+        ease: "power1.out",
+      }
+    );
+  });
+
   return (
     <div className={styles.nameStyle}>
-      <span className={styles.ltr}>K</span>
-      <span className={styles.ltr}>u</span>
-      <span className={styles.ltr}>r</span>
-      <span className={styles.ltr}>o</span>
-      <span className={styles.ltr}>s</span>
-      <span className={styles.ltr}>a</span>
-      <span className={styles.ltr}>w</span>
-      <span className={styles.ltr}>a</span>
+      <span className={`${styles.ltr} ltr`}>K</span>
+      <span className={`${styles.ltr} ltr`}>u</span>
+      <span className={`${styles.ltr} ltr`}>r</span>
+      <span className={`${styles.ltr} ltr`}>o</span>
+      <span className={`${styles.ltr} ltr`}>s</span>
+      <span className={`${styles.ltr} ltr`}>a</span>
+      <span className={`${styles.ltr} ltr`}>w</span>
+      <span className={`${styles.ltr} ltr`}>a</span>
     </div>
   );
 };
