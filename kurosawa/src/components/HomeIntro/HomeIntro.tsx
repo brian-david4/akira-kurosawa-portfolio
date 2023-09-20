6c97941e003f8de33800ad117e0f69d8607d4f07
Brian David <bdavid1530@gmail.com>
2023-09-20 13:14:33 +0100

    Home intro part animations
    - Name fade in and out in english
    - Name fade in and out in japanese
    - Portrait fade in

--- kurosawa/src/components/HomeIntro/HomeIntro.tsx
+++ kurosawa/src/components/HomeIntro/HomeIntro.tsx
@@ -1,15 +1,33 @@
 import { motion } from "framer-motion";
+import { useEffect } from "react";
+import gsap from "gsap";
 import styles from "./HomeIntro.module.css";
+import portrait from "../../images/Kurosawa-Akira-portrait.webp";
 
 const HomeIntro = () => {
+  useEffect(() => {
+    let tl = gsap.timeline();
+    tl.fromTo(
+      ".nameEn",
+      { opacity: 0.7 },
+      { duration: 1.5, opacity: 0, delay: 1.5 }
+    )
+      .fromTo(".nameJp", { opacity: 0 }, { duration: 1.5, opacity: 0.7 })
+      .to(".nameJp", { autoAlpha: 0, duration: 1.5, delay: 1 })
+      .to(".imgPortrait", { duration: 2.5, opacity: 1 });
+  }, []);
+
   return (
     <motion.div className={styles.block}>
+      <div className={`${styles.imgPortrait} imgPortrait`}>
+        <img src={portrait} alt="Headshot of Akira Kurosawa" />
+      </div>
       <div className={styles.name}>
-        <div className={styles.nameEn}>
+        <div className={`${styles.nameEn} nameEn`}>
           <h1>akira</h1>
           <h1>kurosawa</h1>
         </div>
-        <div className={styles.nameJp}>
+        <div className={`${styles.nameJp} nameJp`}>
           <h1>黒沢明</h1>
         </div>
       </div>
