2d81c7506f09306932038f67d26c95879a762d10
Brian David <bdavid1530@gmail.com>
2023-09-22 13:19:42 +0100

    NavImage component position relative to mouse pos

--- kurosawa/src/components/NavImage/NavImage.tsx
+++ kurosawa/src/components/NavImage/NavImage.tsx
@@ -1,18 +1,27 @@
 import { motion } from "framer-motion";
 import styles from "./NavImage.module.css";
 import { navVideo } from "./Anims";
+import useMousePosition from "../../hooks/useMousePosition";
 
 interface NavImageProps {
   src: string;
 }
 
 const NavImage = ({ src }: NavImageProps) => {
+  const { x, y } = useMousePosition();
+  const size = 250;
+
   return (
     <motion.div
       variants={navVideo}
-      initial="initial"
-      animate="enter"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        // left: `${x + "15%"}px`,
+        top: `${y - size}px`,
+      }}
       exit="exit"
+      transition={{ type: "tween", ease: "backOut" }}
       className={styles.navImage}
     >
       <motion.video autoPlay loop={false} draggable={false} playsInline>
