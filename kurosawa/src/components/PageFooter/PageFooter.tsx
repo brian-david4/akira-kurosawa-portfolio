203d230b47a0b03f83fb590944dc7b452aee77cf
Brian David <bdavid1530@gmail.com>
2023-09-24 10:32:46 +0100

    Arrow animation on footer NextLink

--- kurosawa/src/components/PageFooter/PageFooter.tsx
+++ kurosawa/src/components/PageFooter/PageFooter.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./footer.module.css";
+import arrow from "../../images/Arrow.svg";
 
 interface PageFooterProps {
   goTo: string;
@@ -7,11 +9,25 @@ interface PageFooterProps {
 }
 
 const PageFooter = ({ goTo, nextTitle }: PageFooterProps) => {
+  const [rightArrow, setRightArrow] = useState(false);
+
   return (
     <div className={styles.footerMain}>
       <div className={styles.footerInner}>
         <div className={styles.titleNext}>
-          <Link className={styles.link} to={goTo}>
+          <div className={styles.imgWrapper}>
+            <img
+              className={rightArrow ? styles.imgPos : ""}
+              src={arrow}
+              alt="arrow pointing right"
+            />
+          </div>
+          <Link
+            onMouseEnter={() => setRightArrow(true)}
+            onMouseLeave={() => setRightArrow(false)}
+            className={styles.link}
+            to={goTo}
+          >
             {nextTitle}
           </Link>
         </div>
