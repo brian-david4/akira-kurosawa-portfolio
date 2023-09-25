3f0a186e45e1ed26aa143071f742ecf11a7952ea
Brian David <bdavid1530@gmail.com>
2023-09-25 10:24:56 +0100

    Image top and bottom on link hover ->follows mouse

--- kurosawa/src/components/PageFooter/PageFooter.tsx
+++ kurosawa/src/components/PageFooter/PageFooter.tsx
@@ -1,6 +1,8 @@
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./footer.module.css";
+import useMousePosition from "../../hooks/useMousePosition";
 import arrow from "../../images/Arrow.svg";
 import highnLow from "../../images/highlow.webp";
 
@@ -10,38 +12,42 @@ interface PageFooterProps {
 }
 
 const PageFooter = ({ goTo, nextTitle }: PageFooterProps) => {
+  const { x, y } = useMousePosition();
+  const size = 10;
   const [rightArrow, setRightArrow] = useState(false);
 
   return (
     <div className={styles.footerMain}>
-      <div className={styles.footerInner}>
-        <div className={styles.titleNext}>
-          <div className={styles.imgTopWrapper}>
-            <img className={rightArrow ? styles.imgHover : ""} src={highnLow} />
-          </div>
+      <motion.div
+        animate={{ left: `calc(${x}px - ${size}vh)` }}
+        transition={{ type: "tween", ease: "backOut" }}
+        className={styles.imgTopWrapper}
+      >
+        <img className={rightArrow ? styles.imgOpacity : ""} src={highnLow} />
+      </motion.div>
 
-          <div className={styles.imgWrapper}>
-            <img
-              className={rightArrow ? styles.imgPos : ""}
-              src={arrow}
-              alt="arrow pointing right"
-            />
-          </div>
-
-          <Link
-            onMouseEnter={() => setRightArrow(true)}
-            onMouseLeave={() => setRightArrow(false)}
-            className={styles.link}
-            to={goTo}
-          >
-            {nextTitle}
-          </Link>
-
-          <div className={styles.imgBottWrapper}>
-            <img className={rightArrow ? styles.imgHover : ""} src={highnLow} />
-          </div>
-        </div>
+      <div className={styles.linkWrapper}>
+        <Link
+          onMouseEnter={() => setRightArrow(true)}
+          onMouseLeave={() => setRightArrow(false)}
+          className={styles.link}
+          to={goTo}
+        >
+          {nextTitle}
+        </Link>
       </div>
+
+      <motion.div
+        animate={{ left: `calc(${x}px - ${size}vh)` }}
+        transition={{ type: "tween", ease: "backOut" }}
+        className={styles.imgBottWrapper}
+      >
+        <img
+          className={rightArrow ? styles.imgOpacity : ""}
+          style={{ transitionDelay: "0.2s" }}
+          src={highnLow}
+        />
+      </motion.div>
     </div>
   );
 };
