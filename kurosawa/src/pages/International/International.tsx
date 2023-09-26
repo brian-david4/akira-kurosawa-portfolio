ccbdca79e452c70e42c5c1df50744ad5fd8e0006
Brian David <bdavid1530@gmail.com>
2023-09-26 23:47:06 +0100

    Add DecadeText to "/international"
    Also PageSection returns after loading

--- kurosawa/src/pages/International/International.tsx
+++ kurosawa/src/pages/International/International.tsx
@@ -4,6 +4,8 @@ import DecadePageHeader from "../../components/DecadePageHeader/DecadePageHeader
 import KurosawaName from "../../components/KurosawaName/KurosawaName";
 import Signature from "../../components/Signature/Signature";
 import Loading from "../../components/Loading/Loading";
+import PageSection from "../../components/PageSection/PageSection";
+import DecadeText from "../../components/DecadeText/DecadeText";
 
 const International = () => {
   const [loading, setLoading] = useState(true);
@@ -26,6 +28,20 @@ const International = () => {
           </DecadePageHeader>
           <Signature isInView={false} />
         </>
+      )}
+
+      {!loading && (
+        <PageSection>
+          <DecadeText>
+            akira kurosawa came out of the 1940's a success. though often
+            critics would respond with mixed reviews - some liking the work,
+            others saying it was too western for wartime japan - but it was with
+            the audience that he most popular. studios continued funding his
+            films, and kurosawa grew more and more popular in japan. the first
+            year into the new decade would bring kurosawa international
+            recognition and acclaim. he did it with this: <i>Rashomon</i>
+          </DecadeText>
+        </PageSection>
       )}
     </>
   );
