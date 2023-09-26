86939c8cf6901ac4a7a8c57c420adea45a62e603
Brian David <bdavid1530@gmail.com>
2023-09-26 23:12:50 +0100

    Add load timer to "/international"

--- kurosawa/src/pages/International/International.tsx
+++ kurosawa/src/pages/International/International.tsx
@@ -1,16 +1,30 @@
+import { useState, useEffect } from "react";
+
 import DecadePageHeader from "../../components/DecadePageHeader/DecadePageHeader";
 import KurosawaName from "../../components/KurosawaName/KurosawaName";
 import Signature from "../../components/Signature/Signature";
 
 const International = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
   return (
     <>
-      <KurosawaName />
-      <DecadePageHeader>
-        <h1>International</h1>
-        <h1>Success</h1>
-      </DecadePageHeader>
-      <Signature isInView={false} />
+      {!loading && (
+        <>
+          <KurosawaName />
+          <DecadePageHeader>
+            <h1>International</h1>
+            <h1>Success</h1>
+          </DecadePageHeader>
+          <Signature isInView={false} />
+        </>
+      )}
     </>
   );
 };
