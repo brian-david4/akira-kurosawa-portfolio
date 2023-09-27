266764a2a4be501d1b976b37a759f9b2d92ea83d
Brian David <bdavid1530@gmail.com>
2023-09-27 13:16:34 +0100

    Added Text and TwoImage to Greatness

--- kurosawa/src/pages/Greatness/Greatness.tsx
+++ kurosawa/src/pages/Greatness/Greatness.tsx
@@ -8,8 +8,12 @@ import Signature from "../../components/Signature/Signature";
 import Loading from "../../components/Loading/Loading";
 import PageSection from "../../components/PageSection/PageSection";
 import WideImage from "../../components/WideImage/WideImage";
+import DecadeText from "../../components/DecadeText/DecadeText";
+import TwoImage from "../../components/TwoImage/TwoImage";
 // images
 import redBeard02 from "../../images/Greatness/redBeard02.webp";
+import sanjuro00 from "../../images/Greatness/sanjuro00.webp";
+import redBeard01 from "../../images/Greatness/redbeard01.webp";
 
 const Greatness = () => {
   const [loading, setLoading] = useState(true);
@@ -43,6 +47,20 @@ const Greatness = () => {
           <WideImage
             src={redBeard02}
             alt="Bad Sleep Well - A group of journalist look off screen"
+          />
+
+          <DecadeText>
+            the sixties was the most period for <b>akira kurosawa</b>. his movie
+            series consisting of <i>yojimbo</i> and <i>sunjuro</i> were adapted
+            by hollywood as the basis for the western:{" "}
+            <i>A fistful of dollars</i>.
+          </DecadeText>
+
+          <TwoImage
+            src1={sanjuro00}
+            src2={redBeard01}
+            alt1="Sanjuro - A group of samurai bow and kneel infront of the protagonist"
+            alt2="Red Beard - doctors and family members kneel around a sick child"
           />
         </PageSection>
       )}
