921b4953eb05e57d193e9c5e9d8ec44e29a4101b
Brian David <bdavid1530@gmail.com>
2023-09-27 20:29:40 +0100

    Legacy page renders after 2s

--- kurosawa/src/pages/Legacy/Legacy.tsx
+++ kurosawa/src/pages/Legacy/Legacy.tsx
@@ -1,15 +1,31 @@
+import { useState, useEffect } from "react";
+
 import DecadePageHeader from "../../components/DecadePageHeader/DecadePageHeader";
 import KurosawaName from "../../components/KurosawaName/KurosawaName";
 import Signature from "../../components/Signature/Signature";
 
 const Legacy = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
     <>
-      <KurosawaName />
-      <Signature isInView={false} />
-      <DecadePageHeader>
-        <h1>Legacy</h1>
-      </DecadePageHeader>
+      {!loading && (
+        <>
+          <KurosawaName />
+          <Signature isInView={false} />
+          <DecadePageHeader>
+            <h1>Legacy</h1>
+          </DecadePageHeader>
+        </>
+      )}
     </>
   );
 };
