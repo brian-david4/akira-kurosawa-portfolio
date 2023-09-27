5f8546d15fe907cc8b7a606481246c44a76903f9
Brian David <bdavid1530@gmail.com>
2023-09-27 10:45:28 +0100

    Add images to "/international"

--- kurosawa/src/pages/International/International.tsx
+++ kurosawa/src/pages/International/International.tsx
@@ -8,6 +8,13 @@ import Loading from "../../components/Loading/Loading";
 import PageSection from "../../components/PageSection/PageSection";
 import DecadeText from "../../components/DecadeText/DecadeText";
 import MaxImage from "../../components/MaxImage/MaxImage";
+import TwoImage from "../../components/TwoImage/TwoImage";
+import WideImage from "../../components/WideImage/WideImage";
+// images
+import rashomon00 from "../../images/International/rashomon00.webp";
+import rashomon01 from "../../images/International/rashomon01.webp";
+import rashomonbts00 from "../../images/International/rashomonbts00.webp";
+import rashomon02 from "../../images/International/rashomon02.webp";
 
 const International = () => {
   const [loading, setLoading] = useState(true);
@@ -46,6 +53,23 @@ const International = () => {
             year into the new decade would bring kurosawa international
             recognition and acclaim. he did it with this: <i>Rashomon</i>
           </DecadeText>
+
+          <MaxImage
+            src={rashomon00}
+            alt="Rashomon - three men sit in silence listening to the rain"
+          />
+
+          <TwoImage
+            src1={rashomon01}
+            src2={rashomonbts00}
+            alt1="Rashomon - a bandint confronts a woman in a forest"
+            alt2="Rashomon - behind the scene shot of the temple"
+          />
+
+          <WideImage
+            src={rashomon02}
+            alt="Rashomon - Dramtic close up of woman with knife in her hand"
+          />
         </PageSection>
       )}
     </>
