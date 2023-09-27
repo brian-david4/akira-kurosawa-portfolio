04742742af119e2efec4186a36d7675a33d70ea4
Brian David <bdavid1530@gmail.com>
2023-09-27 22:11:32 +0100

    Add all page images to images folder
    Add image in components in Legacy page

--- kurosawa/src/pages/Legacy/Legacy.tsx
+++ kurosawa/src/pages/Legacy/Legacy.tsx
@@ -8,6 +8,18 @@ import Loading from "../../components/Loading/Loading";
 import DecadeNavBar from "../../components/DecadeNavBar/DecadeNavBar";
 import PageSection from "../../components/PageSection/PageSection";
 import DecadeText from "../../components/DecadeText/DecadeText";
+import MaxImage from "../../components/MaxImage/MaxImage";
+import WideImage from "../../components/WideImage/WideImage";
+import TwoImage from "../../components/TwoImage/TwoImage";
+import SingleImage from "../../components/SingleImage/SingleImage";
+import Card from "../../components/Card/Card";
+// images
+import ran04 from "../../images/Legacy/ran04.webp";
+import ran00 from "../../images/Legacy/ran00.webp";
+import ran02 from "../../images/Legacy/ran02.webp";
+import dreams02 from "../../images/Legacy/dreams02.webp";
+import kagebts00 from "../../images/Legacy/kagebts00.webp";
+import ran03 from "../../images/Legacy/ran03.webp";
 
 const Legacy = () => {
   const [loading, setLoading] = useState(true);
@@ -58,6 +70,26 @@ const Legacy = () => {
             and pressured studios to fund his future projects until his death in
             1998
           </DecadeText>
+
+          <WideImage
+            src={ran04}
+            alt="Ran - samurai on horseback galloping through water"
+          />
+
+          <TwoImage
+            src1={ran00}
+            src2={ran02}
+            alt1="Ran - a samurai lord holds council with his sons and advisors"
+            alt2="Ran - billowing clouds on a sunny day"
+          />
+
+          <SingleImage src={ran03} alt="Ran - a shot of green hills" />
+
+          <Card src={kagebts00} alt="">
+            kurosawa behind the scenes filming <i>kagemusha</i>
+          </Card>
+
+          <MaxImage src={dreams02} alt="" />
         </PageSection>
       )}
     </>
