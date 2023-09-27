789a6509ee5b8273b1e352103501077c0c8a9780
Brian David <bdavid1530@gmail.com>
2023-09-27 12:29:50 +0100

    Create 2 second loading time with setTimeout

--- kurosawa/src/pages/Greatness/Greatness.tsx
+++ kurosawa/src/pages/Greatness/Greatness.tsx
@@ -1,17 +1,33 @@
+import { useState, useEffect } from "react";
+
 import DecadeNavBar from "../../components/DecadeNavBar/DecadeNavBar";
 import DecadePageHeader from "../../components/DecadePageHeader/DecadePageHeader";
 import KurosawaName from "../../components/KurosawaName/KurosawaName";
 import Signature from "../../components/Signature/Signature";
 
 const Greatness = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
     <>
-      <KurosawaName />
-      <DecadeNavBar />
-      <Signature isInView={false} />
-      <DecadePageHeader>
-        <h1>Greatness</h1>
-      </DecadePageHeader>
+      {!loading && (
+        <>
+          <KurosawaName />
+          <DecadeNavBar />
+          <Signature isInView={false} />
+          <DecadePageHeader>
+            <h1>Greatness</h1>
+          </DecadePageHeader>
+        </>
+      )}
     </>
   );
 };
