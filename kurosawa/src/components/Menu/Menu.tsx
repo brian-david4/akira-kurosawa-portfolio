8395edc51880ac577e2cd045208b5ac4d4366862
Brian David <bdavid1530@gmail.com>
2023-09-28 12:06:43 +0100

    Change DecadeMenu font size

--- kurosawa/src/components/Menu/Menu.tsx
+++ kurosawa/src/components/Menu/Menu.tsx
@@ -4,16 +4,21 @@ import NavBar from "./NavBar";
 import styles from "./NavBar.module.css";
 
 const Menu = () => {
-    const [isMenuActive, setIsMenuActive] = useState(false); 
+  const [isMenuActive, setIsMenuActive] = useState(false);
 
   return (
     <>
-    <div onClick={()=>setIsMenuActive(!isMenuActive)} className={styles.menuBtn}>
-      <div data-menu-active={isMenuActive} className={styles.burger}></div>
-    </div>
-    <AnimatePresence mode="wait">{isMenuActive && <NavBar />}</AnimatePresence>
+      <div
+        onClick={() => setIsMenuActive(!isMenuActive)}
+        className={styles.menuBtn}
+      >
+        <div data-menu-active={isMenuActive} className={styles.burger}></div>
+      </div>
+      <AnimatePresence mode="wait">
+        {isMenuActive && <NavBar />}
+      </AnimatePresence>
     </>
-  )
-}
+  );
+};
 
 export default Menu;
